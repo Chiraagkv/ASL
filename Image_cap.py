688c1d33dcf4a93fd90f1fa3b4c6dcc46c40decf
rohitkumar9989 <69589867+rohitkumar9989@users.noreply.github.com>
2021-07-18 10:51:28 +0530

    Update Image_cap.py

--- Image_cap.py
+++ Image_cap.py
@@ -2,25 +2,44 @@ import cv2
 import numpy as np
 import random as rd
 import time
+import pickle
+import os
+import sys
+
 
 class Image_detector ():
-    def __init__(self, path, show_boxes=True, thresh_rate=1):
-        self.path=path
-        self.hand_cascade=cv2.CascadeClassifier(f'{self.path}')
+    def __init__(self, show_boxes=True, thresh_rate=1, cascade_path):
+        try:
+            self.cascade_path=cascade_path
+        except Exception as e:
+            print ("Error aoocured while process")
+            sys.exit()
+        self.hand_cascade=cv2.CascadeClassifier(self.cascade_path)
         self.list=[]
         self.show_boxes=show_boxes
         self.thresh_rate=thresh_rate
+        self.crop_list=[]
+        
+        os.removedirs('images_folder')
+        os.remove('predictions.bat')
+
+        try:
+            os.makedirs('images_folder')
+        except Exception as e:
+            pass
+        self.image_processor()
+        self.retrieve_data()
 
     def svae_images (self, frame):
         r=rd.randint(1, 1000)
-        cv2.imwrite("hand_image{}.jpeg".format(str(r)), frame)
+        cv2.imwrite("images_folder\\hand_image{}.jpeg".format(str(r)), frame)
     
     def detect (self, gray_image, orig_frame):
         hand=self.hand_cascade.detectMultiScale(gray_image,3, 2)
         for (x, y, w, h) in hand:
             if self.show_boxes==True:
                 cv2.rectangle(orig_frame, (x,y), (x+w,y+h), (0,255,0), 2)
-                self.list.extend([x, y, w, h])
+                self.list.append([x, y, w, h])
                 time.sleep(self.thresh_rate)
                 self.svae_images(orig_frame)
             else:
@@ -46,6 +65,46 @@ class Image_detector ():
         cv2.destroyAllWindows()
         return self.list
 
+    def retrieve_data (self):
+        self.file_handler()
+        with open('predictions.bat', 'rb') as f:
+                while True:
+                    try:
+                        ans=pickle.load(f)
+                        self.crop_list.append(ans.split('-'))
+                    except EOFError:
+                        break
+        self.crop_images()
 
-a=Image_detector()
-my_boi=a.image_processor()
+    def crop_images(self):
+        os.makedirs('data_full')
+        for m in range (len(self.crop_list)):
+            data=self.crop_list[m][0]
+
+            new_image=cv2.imread(data)
+            y=int(self.crop_list[m][2])
+            x=int(self.crop_list[m][1])
+            h=int(self.crop_list[m][4])
+            w=int(self.crop_list[m][3])
+            
+            new_img=new_image[y:y+h, x:x+w]
+            os.remove(data)
+            cv2.imwrite(data, new_img)
+
+
+    def file_handler (self):
+        with open('predictions.bat', 'wb') as f:
+            for (roots, dirs, files) in os.walk('images_folder'):
+                for i in range (len(files)):
+                    images='images_folder'+'\\'+files[i]
+                    print (self.list[i])
+                    resluts=f"{images}-{self.list[i][0]}-{self.list[i][1]}-{self.list[i][2]}-{self.list[i][3]}"
+                    pickle.dump(resluts, f)
+        f.close()
+
+a=Image_detector(thresh_rate=0.5)
+
+
+
+
+        
