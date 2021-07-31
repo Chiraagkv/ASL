40a2bb8f6052a4ef075abbe36940f5741af66b28
Chiraagkv <69423507+Chiraagkv@users.noreply.github.com>
2021-07-31 19:42:47 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import tensorflow as tf
 import tensorflow_hub as hub
 # import streamlit as st
@@ -53,62 +52,4 @@ def predict_custom(image, model):
 	custom_preds = model.predict(custom_data)
 	conf = f'{np.max(custom_preds[0])* 100:.2f}%'
 	custom_preds_labels = preds_to_text(custom_preds)
-	
-=======
-import tensorflow as tf
-import tensorflow_hub as hub
-# import streamlit as st
-# import matplotlib 
-# import matplotlib.pyplot as plt
-import os
-import pandas
-import numpy as np
-from PIL import Image, ImageOps
-
-# What I have to do:
-
-# 1. Get create_batches
-# 2. Get unbatchify
-# 3. download the model.h5 thing
-# 4. Load it
-# 5. Predict
-# 6. Plot
-# 7. Streamlit stuff
-
-
-breednames = np.array(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q",
-             "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "del", "nothing", "space"])
-IMAGE_SIZE = 200
-
-def process_image(img):
-	data = np.ndarray(shape=(1, 200, 200, 3), dtype=np.float32)
-	image = tf.constant(img, dtype="float32")
-	# image = tf.io.read_file(img)
-	# image = tf.image.decode_jpeg(image, channels=3)
-	image = tf.image.resize(image, [200, 200])
-
-	image_array = np.asarray(image)
-	normalized_image_array = (image_array.astype(np.float32) / 127.0) -1
-
-	data[0] = normalized_image_array
-	return data
-
-def preds_to_text(prediction_proba):
-  return breednames[np.argmax(prediction_proba)]
-
-
-def load_model(model_path):
-  print(f'Loading model from: {model_path}...')
-  model = tf.keras.models.load_model(model_path, 
-                                     custom_objects={"KerasLayer": hub.KerasLayer})
-  return model
-
-def predict_custom(image):
-	model = load_model(model_path='C:\\Users\\abc\\OneDrive\\Desktop\\ASL_deploy\\my_model.h5')
-	custom_data = process_image(image)
-	custom_preds = model.predict(custom_data)
-	conf = f'{np.max(custom_preds[0])* 100:.2f}%'
-	custom_preds_labels = preds_to_text(custom_preds)
-	
->>>>>>> 3c00644c52a294a47f2afc8f9f34a85756725b02
 	return custom_preds_labels, conf
