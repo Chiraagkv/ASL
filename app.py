ed76b506e9de2032c57425dafa136599c5ecd3d5
Chiraagkv <69423507+Chiraagkv@users.noreply.github.com>
2021-08-03 13:01:48 +0530

    this is using mediapipe for hand detection

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import cv2
+import mediapipe as mp
 import numpy as np
 from backend import load_model, predict_custom
-from time import sleep
 import streamlit as st
 
 st.title("ASL to Text")
@@ -17,20 +17,45 @@ except ValueError:
 cap = cv2.VideoCapture(cap_num, cv2.CAP_DSHOW)
 run = st.checkbox('Run')
 
-model = load_model(model_path='./20210711-162248-big-one.h5')
-# Read until q is pressed is completed
+model = load_model(model_path='C:\\Users\\abc\\OneDrive\\Desktop\\ASL_deploy\\my_model.h5')
+
+mphands = mp.solutions.hands
+hands = mphands.Hands()
+mp_drawing = mp.solutions.drawing_utils
+
+
 while run:
-  sleep(0.08)
-  # Capture frame-by-frame
-  ret, frame = cap.read()
-  # Display the resulting frame
-  path = './images/img.jpg'
-  cv2.imwrite(path, frame)
-  pred, conf = predict_custom(image=path, model=model)
-  frame = cv2.putText(frame, f"{pred}, {conf}", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-  # cv2.imshow('Frame', frame)
-  FRAME_WINDOW.image(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
-    # Press Q on keyboard to  exit
+    _, frame = cap.read()
+    h, w, c = frame.shape
+    framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    result = hands.process(framergb)
+    path = 'C:\\Users\\abc\\OneDrive\\Desktop\\ASL_deploy\\images\\img.jpg'
+    cv2.imwrite(path, frame)
+    pred, conf = predict_custom(image=path, model=model)
+    hand_landmarks = result.multi_hand_landmarks
+    if hand_landmarks:
+        for handLMs in hand_landmarks:
+            x_max = 0
+            y_max = 0
+            x_min = w
+            y_min = h
+            for lm in handLMs.landmark:
+                x, y = int(lm.x * w), int(lm.y * h)
+                if x > x_max:
+                    x_max = x
+                if x < x_min:
+                    x_min = x
+                if y > y_max:
+                    y_max = y
+                if y < y_min:
+                    y_min = y
+            cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (130, 255, 20), 5)
+            mp_drawing.draw_landmarks(frame, handLMs, mphands.HAND_CONNECTIONS)
+            frame = cv2.putText(frame, f"{pred}, {conf}", (x_min, y_min - 10), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+            FRAME_WINDOW.image(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+    else:
+        FRAME_WINDOW.image(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+
 else:
   st.markdown("### Check the box to run the app :point_up:")
 
